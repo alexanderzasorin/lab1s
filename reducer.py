1d09eba3c1c15f7fb234e5b4bf249629166f6eb0
alexanderzasorin <bigdata2.0@newprolab.com>
2015-05-18 17:22:25 +0300

    reducer changed reducer_input changed

--- reducer.py
+++ reducer.py
@@ -1,1 +1,23 @@
 __author__ = 'topsolver'
+
+import sys
+
+key = None
+val = 0
+for line in sys.stdin:
+    curkey,curval = line.strip().split(",")
+#    print parts
+    if (curkey != key):
+        if (key != None):
+            print key.strip()+","+str(val)
+        val = float(curval)
+        key = curkey
+
+    else:
+        val += float(curval)
+
+    #print parts[2].strip()+","+parts[2].strip()
+
+if (key != None):
+    print key.strip()+","+str(val)
+

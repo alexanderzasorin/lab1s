bd9991fc32144e8d0d7b23be2180fd0d9b96772f
shtuder <aopichugin@gmail.com>
2015-05-18 17:11:44 +0300

    maybe ready

--- mapper.py
+++ mapper.py
@@ -1,2 +1,9 @@
 __author__ = 'Artem'
+import sys
+da = {}
+for line in sys.stdin:
+    col_ip, col_uid, col_country, col_banner, col_pay = line.strip().split(',')
+    da[col_country] = col_pay
+for i in da:
+    sys.stdout.write(i + ',' + da[i])
 

d9ab037a9aa408aa541b4cc6db80392f96f22978
shtuder <aopichugin@gmail.com>
2015-05-18 17:16:13 +0300

    ready

--- mapper.py
+++ mapper.py
@@ -1,5 +1,7 @@
 __author__ = 'Artem'
 import sys
+#c = open("mapper_input.txt", "r")
+#for line in c:
 for line in sys.stdin:
     col_ip, col_uid, col_country, col_banner, col_pay = line.strip().split(',')
     print (col_country + ',' + col_pay)

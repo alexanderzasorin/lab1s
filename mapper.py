2f5ca7496e8116e247a8b94f26e55142959b4fa7
shtuder <aopichugin@gmail.com>
2015-05-18 17:14:29 +0300

    simpler

--- mapper.py
+++ mapper.py
@@ -1,9 +1,6 @@
 __author__ = 'Artem'
 import sys
-da = {}
 for line in sys.stdin:
     col_ip, col_uid, col_country, col_banner, col_pay = line.strip().split(',')
-    da[col_country] = col_pay
-for i in da:
-    sys.stdout.write(i + ',' + da[i])
+    print (col_country + ',' + col_pay)
 
